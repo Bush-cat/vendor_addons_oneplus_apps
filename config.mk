37a0f2df8486805595b2c26e185331c8050ee53e
chandu078 <chandudyavanapelli03@gmail.com>
2021-02-10 05:35:49 +0000

    oneplus_apps: Update Permissions
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- config.mk
+++ config.mk
@@ -4,5 +4,5 @@ PRODUCT_PACKAGES += \
     OnePlusGallery
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-oem.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-oem.xml \
-    $(LOCAL_PATH)/system/etc/sysconfig/hiddenapi-package-whitelist-oneplus.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hiddenapi-package-whitelist-oneplus.xml
+    $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-oem.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-oem.xml \
+    $(LOCAL_PATH)/system/etc/sysconfig/hiddenapi-package-whitelist-oneplus.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/hiddenapi-package-whitelist-oneplus.xml
